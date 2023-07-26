f554e9b427614cf498808d907680cb7225482044
siudn <cshadx@gmail.com>
2023-07-26 19:13:04 -0400

    Completed 03_reverseString
    Wrote a loop to reverse any string inputted, using length attribute and
    charAt method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverseWord = '';
+    for (let i = (word.length - 1); i > -1; i--) {
+        reverseWord += word.charAt(i);
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
