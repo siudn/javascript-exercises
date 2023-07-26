5bc80e9b347a12e869aeed64ac3ee9368266e69b
siudn <cshadx@gmail.com>
2023-07-26 18:56:50 -0400

    Completed 02_repeatString
    Wrote for loop to repeat a string a given number of times, and if statement
    to return ERROR when given negative number

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (word, num) {
+    if (num < 0) {return "ERROR"};
+    let x = word;
+    let string = "";
+    for (let i = 0; i < num; i++) {
+        string += x;
+    }
+    return string;
 };
 
 // Do not edit below this line
