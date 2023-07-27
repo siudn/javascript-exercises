3d102eadcd14c7265f0b442592ac679b1777817f
siudn <cshadx@gmail.com>
2023-07-26 20:38:11 -0400

    Completed 04_removeFromArray
    Used spread and rest syntax to allow multiple optional arguments, filter
    method to change the array combined with an arrow function excluding
    the arguments that were made

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...args) {
+    return array.filter(x => !args.includes(x));
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
