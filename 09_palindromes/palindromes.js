8a0faa9ef485d25b4acb91cf599d39d17a9e8e0a
siudn <cshadx@gmail.com>
2023-08-01 23:27:45 -0400

    Completed 09_palindromes.js
    Used split, reverse, join. Hated this one.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = str => {
+    let newStr = str.toLowerCase().replace(/[^a-z0-9]+/g, '');
+    const arr = newStr.split('');
+    const revArr = arr.reverse();
+    if (newStr === revArr.join('')) {
+        return true;
+    } else
+        return false;
 };
 
 // Do not edit below this line
