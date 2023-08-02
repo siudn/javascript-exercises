8f82236be948de62351f6cd96874892f1b53558a
siudn <cshadx@gmail.com>
2023-08-02 04:59:28 -0400

    Completed all javascript exercises

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = people => {
+    return people.reduce((oldest, current) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
 };
+
+const getAge = (birthyear, deathyear) => {
+    if (!deathyear) {
+        deathyear = new Date().getFullYear();
+    }
+    return deathyear - birthyear;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
