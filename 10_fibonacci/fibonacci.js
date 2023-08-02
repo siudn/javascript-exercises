f39c27ff51ee0c0a2cdc19506fd88d5cb1443bd4
siudn <cshadx@gmail.com>
2023-08-02 03:08:21 -0400

    For loop and array methods to create fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,7 +1,7 @@
 const fibonacci = n => {
     const fibo = [0, 1];
     if (n < 0)
-        return "OOPS"
+        return "OOPS";
     for (i = 1; i <= n; i++) {
         fibo.push(fibo[i] + fibo[i - 1]);
     }
