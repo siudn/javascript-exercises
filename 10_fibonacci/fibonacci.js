dcdc38e6727017acbd66b1d7d1a5df143844db2f
siudn <cshadx@gmail.com>
2023-08-02 03:04:37 -0400

    Done - coded fibonacci sequence
    Done using a for loop and array methods. Code is pretty efficient.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = n => {
+    const fibo = [0, 1];
+    if (n < 0)
+        return "OOPS"
+    for (i = 1; i <= n; i++) {
+        fibo.push(fibo[i] + fibo[i - 1]);
+    }
+    return fibo[n];
 };
 
 // Do not edit below this line
